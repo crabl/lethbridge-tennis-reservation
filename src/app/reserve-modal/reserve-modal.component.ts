b0521da9d5a21d699b3028bab4428255e9fc0c88
Chris Rabl <rabl.christopher@gmail.com>
2020-05-17 12:57:46 -0600

    manage expectations

--- src/app/reserve-modal/reserve-modal.component.ts
+++ src/app/reserve-modal/reserve-modal.component.ts
@@ -43,7 +43,7 @@ import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
         </div>
 
         <div class="form-group">
-          <p class="text-left">Once your timeslot request is confirmed, you will receive an e-mail. Please note that only registered Lethbridge Tennis Club members can reserve timeslots through this form. <a href="http://www.tennislethbridge.ca/membership.html">To purchase a membership, go here.</a></p>
+          <p class="text-left">Once your timeslot request is confirmed, it will show up in green on the schedule. Please note that only registered Lethbridge Tennis Club members can reserve timeslots through this form. <a href="http://www.tennislethbridge.ca/membership.html">To purchase a membership, go here.</a></p>
         <div>
       </div>
 
